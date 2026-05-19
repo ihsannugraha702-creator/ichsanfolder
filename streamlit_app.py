2f30637211ecc859f7acccff4c1610bf470ed38e
ihsannugraha702-creator <ihsannugraha702@gmail.com>
2026-05-19 11:14:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈aplikasi garut")
+st.title("🎈ichsanfolder")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
