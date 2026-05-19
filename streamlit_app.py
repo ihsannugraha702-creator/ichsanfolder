ec3099bd1fb4ba4960bdc01f8ee09b465d8b87f8
ihsannugraha702-creator <ihsannugraha702@gmail.com>
2026-05-19 11:20:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,24 @@ st.latex(r'''
     \sum_{k=0}^{n-1} ar^k =
     a \left(\frac{1-r^{n}}{1-r}\right)
     ''')
+import streamlit as st
+
+def get_user_name():
+    return 'Anin'
+
+with st.echo():
+    # Everything inside this block will be both printed to the screen
+    # and executed.
+
+    def get_punctuation():
+        return '!!!'
+
+    greeting = "Hi there, "
+    value = get_user_name()
+    punctuation = get_punctuation()
+
+    st.write(greeting, value, punctuation)
+
+# And now we're back to _not_ printing to the screen
+foo = 'bar'
+st.write('Done!')
