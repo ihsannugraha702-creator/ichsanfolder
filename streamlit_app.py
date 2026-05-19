a3af20785dc173465ea03a8290e61cbe552621f1
ihsannugraha702-creator <ihsannugraha702@gmail.com>
2026-05-19 11:17:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,8 @@ import streamlit as st
 
 st.markdown("*Streamlit* is **really** ***cool***.")
 st.markdown('''
-    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
-    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
+    :red[Streamlit] :orange[habil] :green[orang] :blue[garut] :violet[asli]
+    :gray[habil] :rainbow[maung] and :blue-background[bandung] text.''')
 st.markdown("Here's a bouquet &mdash;\
             :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
 
