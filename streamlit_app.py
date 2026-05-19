162bf159423ba3d362b2ab2ec9af93a7ca39518a
ihsannugraha702-creator <ihsannugraha702@gmail.com>
2026-05-19 11:15:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,7 @@ a soft return is used for the next line.
 Two (or more) newline characters in a row will result in a hard return.
 '''
 st.markdown(multi)
+import streamlit as st
+
+st.caption("This is a string that explains something above.")
+st.caption("A caption with _italics_ :blue[colors] and emojis :sunglasses:")
